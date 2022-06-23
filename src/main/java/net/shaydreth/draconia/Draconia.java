aa806be4143d2758b54dff6cd5671127ff6554aa
Shaydreth <firefoxdemon_101@yahoo.com>
2022-06-23 15:10:15 -0500

    Commit Project

--- src/main/java/net/shaydreth/draconia/Draconia.java
+++ src/main/java/net/shaydreth/draconia/Draconia.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+//Testing Learning Important Comment
 public class Draconia implements ModInitializer {
 	public static final String MOD_ID = "draconia";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
