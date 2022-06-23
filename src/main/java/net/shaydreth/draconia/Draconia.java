87041704ec3a5613339103cb89408684927cd5e8
Shaydreth <firefoxdemon_101@yahoo.com>
2022-06-23 15:03:18 -0500

    Commit Project

---
+++ src/main/java/net/shaydreth/draconia/Draconia.java
@@ -0,0 +1,15 @@
+package net.shaydreth.draconia;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Draconia implements ModInitializer {
+	public static final String MOD_ID = "draconia";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/shaydreth/draconia/DraconiaClient.java
@@ -0,0 +1,10 @@
+package net.shaydreth.draconia;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DraconiaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
